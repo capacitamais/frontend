b19a443ff00a1ce145623eca41350d379e08eab7
Daniel Andrade Silva <danielandradesilva.84@gmail.com>
2025-05-18 15:58:22 -0300

    Ajustando barra de rolagem

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,36 @@
-import './style.css'
-import Navbar from '../Navbar/Navbar';
-import Footer from '../Footer/Footer';
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar'; // Supondo que o caminho está correto
+import Footer from '../Footer/Footer'; // Supondo que o caminho está correto
+import './style.css';
 
 export default function Layout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const updateMainContentHeight = () => {
+      const navbarElement = document.querySelector('.navbar');
+      const footerElement = document.querySelector('.footer');
+      const mainContentElement = document.querySelector('.main-content');
+
+      if (navbarElement && footerElement && mainContentElement) {
+        const navbarHeight = navbarElement.offsetHeight;
+        const footerHeight = footerElement.offsetHeight;
+        const newMaxHeight = `calc(100vh - ${navbarHeight}px - ${footerHeight}px)`;
+        mainContentElement.style.maxHeight = newMaxHeight;
+      }
+    };
+
+    // Chame a função inicialmente e sempre que a janela for redimensionada
+    updateMainContentHeight();
+    window.addEventListener('resize', updateMainContentHeight);
+
+    // Limpeza do listener no desmontar do componente
+    return () => {
+      window.removeEventListener('resize', updateMainContentHeight);
+    };
+  }, [location]); // Dependência do location para atualizar em mudanças de rota, se necessário
+
   return (
     <div className='app-container'>
       <Navbar />
